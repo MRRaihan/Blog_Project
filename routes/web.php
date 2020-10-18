4b150102cb6ef7c326b9770e55bba089d7794de5
mizanur <mizanurahmed1452@gmail.com>
2020-10-18 13:20:00 +0600

    Create controller, route and view for dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Admin Panel Routes
+
+Route::group(['prefix'=>'admin'], function (){
+    Route::get('dashboard', 'DashboardController@dashboard')->name('admin.dashboard');
+
+});
+
 
 Auth::routes();
 
